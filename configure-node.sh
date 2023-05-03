1512ebed7065b9f1ee17ec911ea202080e17297e
Anatoly Shulik <112688813+Mooncess@users.noreply.github.com>
2023-05-03 19:00:14 +0300

    Update configure-node.sh

--- configure-node.sh
+++ configure-node.sh
@@ -4,6 +4,6 @@
 
 sleep 10
 
-/opt/couchbase/bin/couchbase-cli cluster-init -c localhost --cluster-username=$COUCHBASE_ADMIN_USERNAME --cluster-password=$COUCHBASE_ADMIN_PASSWORD --services=data,index,query --cluster-ramsize=1024 --index-storage-setting=default
+/opt/couchbase/bin/couchbase-cli cluster-init -c dockercouchbase.onrender.com --cluster-username=$COUCHBASE_ADMIN_USERNAME --cluster-password=$COUCHBASE_ADMIN_PASSWORD --services=data,index,query --cluster-ramsize=1024 --index-storage-setting=default
 
-/opt/couchbase/bin/couchbase-cli bucket-create -c localhost --username=$COUCHBASE_ADMIN_USERNAME --password=$COUCHBASE_ADMIN_PASSWORD --bucket=default --bucket-type=couchbase --bucket-ramsize=512 --bucket-replica=1 --enable-flush=1
+/opt/couchbase/bin/couchbase-cli bucket-create -c dockercouchbase.onrender.com --username=$COUCHBASE_ADMIN_USERNAME --password=$COUCHBASE_ADMIN_PASSWORD --bucket=default --bucket-type=couchbase --bucket-ramsize=512 --bucket-replica=1 --enable-flush=1
